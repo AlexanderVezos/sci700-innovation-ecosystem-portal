ad61af7b3e07e179fb16f4d4b8e5afdb55336755
Alexander <vez17@outlook.com>
2026-05-09 10:19:50 +1000

    extra fix

--- server/index.js
+++ server/index.js
@@ -33,6 +33,6 @@ app.use(
 );
 
 app.use(express.static(path.join(__dirname, "../dist")));
-app.get("*", (_req, res) => res.sendFile(path.join(__dirname, "../dist/index.html")));
+app.get("/{*path}", (_req, res) => res.sendFile(path.join(__dirname, "../dist/index.html")));
 
 app.listen(3002, () => console.log("Server running on http://localhost:3002"));
